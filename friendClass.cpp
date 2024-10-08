4c44cb9466e1821cd6357627d1a7d60919150bbd
devilRaik <litthf@gmail.com>
2024-10-08 10:29:19 +0530

    Friend Class Lesson

--- friendClass.cpp
+++ friendClass.cpp
@@ -1,55 +1,63 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // Forward Declaration
 class Complex;
 
-class Calculator{
-    public:
-        int add(int a, int b){
-            return (a+b);
-        }
-        int sumRealComplex(Complex, Complex);
-        int sumComComplex(Complex, Complex);
+class Calculator
+{
+public:
+    int add(int a, int b)
+    {
+        return (a + b);
+    }
+    int sumRealComplex(Complex, Complex);
+    int sumCompComplex(Complex, Complex);
 };
 
+class Complex
+{
+    int a, b;
+    // Individually declaring function as friends
+    // friend int Calculator ::sumRealComplex(Complex, Complex);
+    // friend int Calculator ::sumCompComplex(Complex, Complex);
+    // Aliter: Declaring the entire calculator class as fiend
 
-class Complex{
-    int a,b;
-    
-    // Individually declaring functions as friend
-        // friend int Calculator :: sumRealComplex(Complex, Complex);
-        // friend int Calculator :: sumComComplex(Complex, Complex);
+    friend class Calculator;
 
-    // Aliter: Declaring the entire calculator class as friend
-        friend class Calculator;
-    public:
-        int setNum(int v1, int v2){
-            a = v1;
-            b = v2;
-        }
+public:
+    int setNum(int v1, int v2)
+    {
+        a = v1;
+        b = v2;
+    }
 
-        void printNum(){
-            cout<<"This complex number is : "<<a<<" + "<<b<<"i"<<endl;
-        }
+    void printNum()
+    {
+        cout << "The Complex Number is " << a << " + " << b << "i" << endl;
+    }
 };
-int Calculator :: sumRealComplex(Complex o1, Complex o2){
-        return (o1.a + o2.a);
+
+int Calculator ::sumRealComplex(Complex o1, Complex o2)
+{
+    return (o1.a + o2.a);
 }
 
-int Calculator :: sumComComplex(Complex o1, Complex o2){
-        return (o1.b + o2.b);
+int Calculator ::sumCompComplex(Complex o1, Complex o2)
+{
+    return (o1.b + o2.b);
 }
 
-
-int main(){
-    Complex o1, o2;
-    o1.setNum(3,5);
-    o2.setNum(4,6);
+int main()
+{
+    Complex c1, c2;
+    c1.setNum(5, 6);
+    c2.setNum(9, 5);
     Calculator calc;
-    int res = calc.sumRealComplex(o1, o2);
-    int res1 = calc.sumComComplex(o1, o2);
-    cout<<"The sum of real part of O1 and O2 is "<<res<<endl;
-    cout<<"The sum of real other part of O1 and O2 is "<<res1<<endl;
+    int res = calc.sumRealComplex(c1, c2);
+    cout << "The sum of real part of O1 and O2 is " << res << endl;
+    int resc = calc.sumCompComplex(c1, c2);
+    cout << "The sum of Complex part of O1 and O2 is " << resc << endl;
+
     return 0;
 }
